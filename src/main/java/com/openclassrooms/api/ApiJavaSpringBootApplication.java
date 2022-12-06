5b609a92760aa6569a60a621c6fedde8b4684730
Gregory Caloin <gregchapauli@gmail.com>
2022-12-06 09:18:13 +0100

    first commit

---
+++ src/main/java/com/openclassrooms/api/ApiJavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJavaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJavaSpringBootApplication.class, args);
+	}
+
+}
